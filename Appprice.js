2769e6efe8e34d46602fc2aa033ec1769ccae694
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-12-22 17:58:22 +0700

    785501983add

--- Appprice.js
+++ Appprice.js
@@ -1,7 +1,7 @@
 /*using surge cron*/
 
 const region = "vn"
-const appIds = ["1261944766","436577167","641613694","1312014438"]
+const appIds = ["785501983","436577167","641613694","1312014438"]
 
 var cacheData = $persistentStore.read()
 if (!cacheData) {
