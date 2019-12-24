6853e16a1b657d74b610cbd9dba666d27f7a0952
Lãng Khách <langkhach270389@users.noreply.github.com>
2019-12-24 10:38:00 +0700

    fix zingmp3.js

--- zingmp3.js
+++ zingmp3.js
@@ -1,4 +1,2 @@
-body = $response.body.replace(/"lastVipExpireTime".*\d{10}\n/, "");
-let obj = JSON.parse(body);
-obj["data"]["vip"]= {"expireTime":1869254270,"startTime":1572527803,"subscription":{"status":1,"expireTime": 1869254270,"platform":2},"vipType":1};
-$done({body: JSON.stringify(obj)});
+body = $response.body.replace(/"lastVipExpireTime":+\d{10}/g, "\"vip\":{\"expireTime\":32500966600,\"startTime\":1572527803,\"subscription\":{\"status\":1,\"expireTime\": 1869254270,\"platform\":2},\"vipType\":1}");
+$done({body});
