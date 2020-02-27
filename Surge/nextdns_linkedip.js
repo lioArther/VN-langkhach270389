3b87df8b4e516bdc7fa84b922e5c5354c25e5e65
Lãng Khách <langkhach270389@users.noreply.github.com>
2020-02-27 08:43:27 +0700

    nextdns_linkedip.js

--- Surge/nextdns_linkedip.js
+++ Surge/nextdns_linkedip.js
@@ -2,7 +2,7 @@
 
 $httpClient.get('https://link-ip.nextdns.io/3994b1/40bcbadb4693c2a5', function(error, response, data){
   if (error) {
-$notification.post('NEXT DNS ', 'Internet error','');
+//$notification.post('NEXT DNS ', 'Internet error','');
     $done({});
   } else {
 $notification.post('NEXT DNS ', 'IPv4 (with linked IP)', 'ip :' + data);
